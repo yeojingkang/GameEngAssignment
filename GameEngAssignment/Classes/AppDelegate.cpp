514d1620911dc6007ae4788dc4aa991afeefcd84
lamweilun <calamitysir@gmail.com>
2015-05-19 11:39:58 +0800

    Commit gameScene

--- GameEngAssignment/Classes/AppDelegate.cpp
+++ GameEngAssignment/Classes/AppDelegate.cpp
@@ -1,5 +1,6 @@
 #include "AppDelegate.h"
 #include "HelloWorldScene.h"
+#include "gameScene.h"
 
 USING_NS_CC;
 
@@ -47,7 +48,7 @@ bool AppDelegate::applicationDidFinishLaunching() {
     register_all_packages();
 
     // create a scene. it's an autorelease object
-    auto scene = HelloWorld::createScene();
+    auto scene = GameWorld::createScene();
 
     // run
     director->runWithScene(scene);
---
+++ GameEngAssignment/Classes/gameScene.cpp
@@ -0,0 +1,52 @@
+#include "gameScene.h"
+
+USING_NS_CC;
+
+Scene* GameWorld::createScene()
+{
+    // 'scene' is an autorelease object
+    auto scene = Scene::create();
+    
+    // 'layer' is an autorelease object
+	auto layer = GameWorld::create();
+
+    // add layer as a child to scene
+    scene->addChild(layer);
+
+    // return the scene
+    return scene;
+}
+
+// on "init" you need to initialize your instance
+bool GameWorld::init()
+{
+    //////////////////////////////
+    // 1. super init first
+    if ( !Layer::init() )
+    {
+        return false;
+    }
+    
+    Size visibleSize = Director::getInstance()->getVisibleSize();
+    Vec2 origin = Director::getInstance()->getVisibleOrigin();
+
+	
+   
+    
+    return true;
+}
+
+
+void GameWorld::menuCloseCallback(Ref* pSender)
+{
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_WP8) || (CC_TARGET_PLATFORM == CC_PLATFORM_WINRT)
+	MessageBox("You pressed the close button. Windows Store Apps do not implement a close button.","Alert");
+    return;
+#endif
+
+    Director::getInstance()->end();
+
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
+    exit(0);
+#endif
+}

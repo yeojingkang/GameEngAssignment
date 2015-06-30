bb14b1ce9f22c7e4c9005355b7460f16579ae4b9
Yeo Jing Kang <yeojingkang@hotmail.com>
2015-06-30 12:10:29 +0800

    Fixed some subwaves still spawning monsters after having finished spawning all the monsters inside.

--- GameEngAssignment/Classes/Subwave.cpp
+++ GameEngAssignment/Classes/Subwave.cpp
@@ -15,12 +15,17 @@ void CSubwave::init(float activateTime, int normal){
 	this->spawnTimer = 0.0f;
 }
 CEnemy* CSubwave::update(float dt, cocos2d::Vec2 playerPosition){
+	//If there is no more monster left to spawn, end 
+	if (getTotalMonsters() <= 0)
+		return NULL;
+
 	spawnTimer += dt;
 
 	if (spawnTimer >= 1.0f){
 		//Spawn monster
 		CEnemy* newEnemy = new CEnemy();
 		//Determine enemy type, currently only normal monsters
+
 		newEnemy->Init(playerPosition);
 		--normal;
 		spawnTimer = 0.0f;

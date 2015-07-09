53a9ddf90bf641bc1ca74de593054ddf7876ebe0
Yeo Jing Kang <yeojingkang@hotmail.com>
2015-07-09 12:43:12 +0800

    Fixed spawning an undefined enemy type using waves.txt which causes the game to crash.

--- GameEngAssignment/Classes/gameScene.cpp
+++ GameEngAssignment/Classes/gameScene.cpp
@@ -516,6 +516,10 @@ void GameWorld::update(float dt)
 			for (vector<int>::iterator itr = spawnList.begin(); itr != spawnList.end(); ++itr){
 				int index = std::distance(spawnList.begin(), itr);
 
+				//If an enemy is being spawned where it's type isn't defined, break
+				if (index + 1 > theTypes.size())
+					break;
+
 				//Spawn number of enemies for each enemy type
 				for (int i = 0; i < spawnList[index]; ++i){
 					CEnemy* newEnemy = new CEnemy();
